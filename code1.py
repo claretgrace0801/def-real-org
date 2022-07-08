78c276e17018d5177af89ff025d88b1860018f6d
shreyas <shpradhan12@gmail.com>
2022-07-08 18:45:44 +0530

    del

---
+++ code1.py
@@ -0,0 +1,17 @@
+a = 10
+b = 20
+
+print(a+b)
+
+print("fuc ya new feature 1")
+
+print("started feature-4")
+print("changed stuff and finished to feature-4")
+
+print("new stuff in feature 4 after merging")
+
+print("hello there I am making changes here like a retard in to main branch")
+
+print("pls delete the woohoo dir")
+
+print("mat delete karo bhai, ye feature hai")
